2a3be90904a5d86649a0fb2261fb8b57fddcc289
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-09-22 13:19:38 +0200

    Modification of prog.cs

--- prog.cs
+++ prog.cs
@@ -11,60 +11,85 @@ namespace Les_Fonctions
         static void Main(string[] args)
         {
             bool boucleConitnue = true; 
+       
             while (boucleConitnue)
-            { 
+         
+            {
+                // Choisir une opération 
+                Console.WriteLine("Choisir une opération valide (addition, soustraction, multiplication, soustraction, modulo) : "  );
+                string operation = Console.ReadLine().ToLower();
+
                 // Addition 
-                Console.WriteLine("entrer le premier nombre a : ");
-                float premierN = float.Parse(Console.ReadLine());
-                Console.WriteLine("entrer le second nombre b : ");
-                float secondN = float.Parse(Console.ReadLine());
-                Console.WriteLine("En additionant les deux nombres, on obtient : ");
-                Console.WriteLine(premierN + "+" + secondN + "= " + Add(premierN, secondN));
-                Console.ReadLine();
+                if (operation == "addition")
+                {
+                    Console.WriteLine("entrer le premier nombre a : ");
+                    float premierN = float.Parse(Console.ReadLine());
+                    Console.WriteLine("entrer le second nombre b : ");
+                    float secondN = float.Parse(Console.ReadLine());
+                    Console.WriteLine("En additionant les deux nombres, on obtient : ");
+                    Console.WriteLine(premierN + "+" + secondN + "= " + Add(premierN, secondN));
+                    Console.ReadLine();
+                }
 
                 // Soustraction
-                Console.WriteLine("entrer le premier nombre a : ");
-                float soustactionA = float.Parse(Console.ReadLine());
-                Console.WriteLine("entrer le second nombre b : ");
-                float soustractionB = float.Parse(Console.ReadLine());
-                Console.WriteLine("En soustrayant les deux nombres, on obtient : ");
-                Console.WriteLine(soustactionA + "-" + soustractionB + "= " + Substract(soustactionA, soustractionB));
-                Console.ReadLine();
+                else if (operation == "soustraction")
+                {
+                    Console.WriteLine("entrer le premier nombre a : ");
+                    float soustactionA = float.Parse(Console.ReadLine());
+                    Console.WriteLine("entrer le second nombre b : ");
+                    float soustractionB = float.Parse(Console.ReadLine());
+                    Console.WriteLine("En soustrayant les deux nombres, on obtient : ");
+                    Console.WriteLine(soustactionA + "-" + soustractionB + "= " + Substract(soustactionA, soustractionB));
+                    Console.ReadLine();
+                }
 
                 // Multiplication
-                Console.WriteLine("entrer le premier nombre a : ");
-                float multiplicationA = float.Parse(Console.ReadLine());
-                Console.WriteLine("entrer le second nombre b : ");
-                float multiplicationB = float.Parse(Console.ReadLine());
-                Console.WriteLine("En multipliant les deux nombres, on obtient : ");
-                Console.WriteLine(multiplicationA + "*" + multiplicationB + "= " + Multiply(multiplicationA, multiplicationB));
-                Console.ReadLine();
+                else if (operation == "multiplication")
+                {
+                    Console.WriteLine("entrer le premier nombre a : ");
+                    float multiplicationA = float.Parse(Console.ReadLine());
+                    Console.WriteLine("entrer le second nombre b : ");
+                    float multiplicationB = float.Parse(Console.ReadLine());
+                    Console.WriteLine("En multipliant les deux nombres, on obtient : ");
+                    Console.WriteLine(multiplicationA + "*" + multiplicationB + "= " + Multiply(multiplicationA, multiplicationB));
+                    Console.ReadLine();
+                }
 
                 // Division 
-                Console.WriteLine("entrer le premier nombre a : ");
-                float divisionA = float.Parse(Console.ReadLine());
-                Console.WriteLine("entrer le second nombre b : ");
-                float divisionB = float.Parse(Console.ReadLine());
-                Console.WriteLine("En divisant les deux nombres, on obtient : ");
-                Console.WriteLine(multiplicationA + "*" + multiplicationB + "= " + Divide(divisionA, divisionB));
-                Console.ReadLine();
+                else if (operation == "division")
+                {
+                    Console.WriteLine("entrer le premier nombre a : ");
+                    float divisionA = float.Parse(Console.ReadLine());
+                    Console.WriteLine("entrer le second nombre b : ");
+                    float divisionB = float.Parse(Console.ReadLine());
+                    Console.WriteLine("En divisant les deux nombres, on obtient : ");
+                    Console.WriteLine(divisionA + "*" + divisionB + "= " + Divide(divisionA, divisionB));
+                    Console.ReadLine();
+                }
 
                 // Modulo 
-                Console.WriteLine("entrer le premier nombre a : ");
-                int moduloA = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("entrer le second nombre b : ");
-                int moduloB = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("Avec la fonction Modulo, on peut trouver le reste de la division euclidienne de a par b ");
-                Console.WriteLine(moduloA + "%" + moduloB + "= " + Modulo(moduloA, moduloB));
-                Console.ReadLine();
-                
+                else if (operation == "modulo")
+                {
+                    Console.WriteLine("entrer le premier nombre a : ");
+                    int moduloA = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("entrer le second nombre b : ");
+                    int moduloB = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Avec la fonction Modulo, on peut trouver le reste de la division euclidienne de a par b ");
+                    Console.WriteLine(moduloA + "%" + moduloB + "= " + Modulo(moduloA, moduloB));
+                    Console.ReadLine();
+                }
 
-              }
+                else
+                {
+                    Console.WriteLine("Impossible de faire l'opération sélectionnée.");
+                    Console.ReadLine();
+                }
 
 
             }
-            
-            
+
+
+            }
         // Add
             static float Add(float a, float b)
         {
@@ -94,8 +119,6 @@ namespace Les_Fonctions
         {
             return a % n; 
         }
-        
-        // Je sais que j'aurai pu mettre les mêmes noms de variables pour chacune des fonctions. C'est pour éviter de me perdre au max
 
     }
 
